d6de13ff65c2104a3bda08e0c8ff7f154e457369
masha <masha@LAPTOP-9UV7MQD7>
2026-03-29 02:12:46 +0200

    Merge origin/main

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // заменяем на тестовый проект
-const supabaseUrl = "https://rvfzblgemdjfyrfaktws.supabase.co";
-const supabaseKey = "sb_publishable_CHsxF2C8LQ9t2bYb9xDEJZkz";
+const supabaseUrl = "https://zzycxtlxhrtgcfdzgcsb.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp6eWN4dGx4aHJ0Z2NmZHpnY3NiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ3MDg0MzksImV4cCI6MjA5MDI4NDQzOX0.HdgX9ErkXLtUCOIO20TUPor0lz3kHI_JHQe7AFq42B8";
 
 // создаём клиент
 window.supabaseClient = window.supabaseClient || window.supabase.createClient(supabaseUrl, supabaseKey);
