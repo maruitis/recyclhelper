65ad14f5a0c23e333d7d6a902336ade34a26c062
masha <masha@LAPTOP-9UV7MQD7>
2026-03-29 02:22:47 +0200

    Merge origin/main

--- js/main.js
+++ js/main.js
@@ -1,14 +1,11 @@
-// заменяем на тестовый проект
 const supabaseUrl = "https://zzycxtlxhrtgcfdzgcsb.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp6eWN4dGx4aHJ0Z2NmZHpnY3NiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ3MDg0MzksImV4cCI6MjA5MDI4NDQzOX0.HdgX9ErkXLtUCOIO20TUPor0lz3kHI_JHQe7AFq42B8";
 
-// создаём клиент
-window.supabaseClient = window.supabaseClient || window.supabase.createClient(supabaseUrl, supabaseKey);
-const supabase = window.supabaseClient;
+// Используем просто window.supabase для всех скриптов
+window.supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 
-// тестовый запрос
 (async function test() {
-  const { data, error } = await supabase.from('todos').select('*');
-  if (error) console.error(error);
+  const { data, error } = await window.supabase.from('todos').select('*');
+  if (error) console.error("Test error:", error);
   else console.log("Todo data:", data);
 })();
